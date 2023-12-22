9f750ab019faafaa1060822f97975c098a11690c
Alexander Wachowiak <ge96let@mytum.de>
2023-12-22 16:18:58 +0100

    The snapping generally works, however there are some bugs where it sometimes doensn't snap when going to the edge, sometimes it also snaps to often

--- core/src/de/tum/cit/ase/maze/GameScreen.java
+++ core/src/de/tum/cit/ase/maze/GameScreen.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import static de.tum.cit.ase.maze.Direction.*;
+
 /**
  * The GameScreen class is responsible for rendering the gameplay screen.
  * It handles the game logic and rendering of the game elements.
@@ -62,16 +64,16 @@ public class GameScreen implements Screen {
         Player player = game.getGameEngine().getPlayer();
 
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            player.setDirection(Direction.UP);
+            player.setDirection(UP);
             renderPlayer();
         } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            player.setDirection(Direction.DOWN);
+            player.setDirection(DOWN);
             renderPlayer();
         } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            player.setDirection(Direction.LEFT);
+            player.setDirection(LEFT);
             renderPlayer();
         } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            player.setDirection(Direction.RIGHT);
+            player.setDirection(RIGHT);
             renderPlayer();
         } else {
             // Do not change the direction here, just render the standing player
@@ -79,11 +81,12 @@ public class GameScreen implements Screen {
         }
 
 
+        if(isPlayerAtEdge()){
+            System.out.println("Player at Edge; Updating Camera");
+            updateCameraPosition();
+        }
 
-        camera.update(); // Update the camera
-
-
-
+        //camera.update(); // Update the camera
 
         // Move text in a circular path to have an example of a moving object
         sinusInput += delta;
@@ -104,8 +107,6 @@ public class GameScreen implements Screen {
         MapObject[][] mapObjects = game.getGameEngine().getStaticGameMap().getStaticMapObjects();
 
         // Calculate the size of each tile on the screen, for example:
-
-
         game.getSpriteBatch().begin();
 
         for (int row = 0; row < mapObjects.length; row++) {
@@ -181,14 +182,98 @@ public class GameScreen implements Screen {
 
     private Direction getStandingDirection(Direction currentDirection) {
         return switch (currentDirection) {
-            case UP -> Direction.STANDINGUP;
-            case DOWN -> Direction.STANDINGDOWN;
-            case LEFT -> Direction.STANDINGLEFT;
-            case RIGHT -> Direction.STANDINGRIGHT;
+            case UP -> STANDINGUP;
+            case DOWN -> STANDINGDOWN;
+            case LEFT -> STANDINGLEFT;
+            case RIGHT -> STANDINGRIGHT;
             // For standing directions, keep the same
             default -> currentDirection;
         };
     }
+
+    private boolean isPlayerAtEdge() {
+        Player player = game.getGameEngine().getPlayer();
+        float playerX = player.getCurrentWindowX();
+        float playerY = player.getCurrentWindowY();
+
+        float edge = 0.2f;
+        float playerWidth = 16;  // Replace with actual player width
+        float playerHeight = 32; // Replace with actual player height
+
+        // Calculate the bounds of the camera's viewport
+        float leftBound = camera.position.x - camera.viewportWidth / 2;
+        float rightBound = camera.position.x + camera.viewportWidth / 2;
+        float bottomBound = camera.position.y - camera.viewportHeight / 2;
+        float topBound = camera.position.y + camera.viewportHeight / 2;
+
+        System.out.println("LeftBound:"+leftBound);
+
+        System.out.println("Camera Coords:"+camera.position.x+ " "+ camera.position.y);
+        System.out.println("Camera Viewport"+camera.viewportWidth);
+        System.out.println("PlayerCoords"+playerX + " "+playerY);
+
+
+        // Check left and right bounds
+        if (playerX < 0 + camera.position.x * edge/2 && playerX > 0) {
+            return true;
+        } else if (playerX > camera.position.x * 2 - camera.position.x * edge/2) {
+            return true;
+        }
+
+        System.out.println("top edge: " + camera.position.y * edge);
+        float val = camera.position.y * 2 -camera.position.y * edge;
+        System.out.println("bottom edge: " +val );
+        // Check top and bottom bounds
+        if (playerY < 0 + camera.position.y * edge) {
+            return true;
+        } else if (playerY  > camera.position.y * 2 -camera.position.y * edge) {
+            System.out.println("bottom edge: " +val );
+            return true;
+        }
+
+        return false;
+    }
+
+    // Reason for the "snapping"-bug is because I work with percentages.
+    private void updateCameraPosition() {
+        Player player = game.getGameEngine().getPlayer();
+        float screenWidth = camera.viewportWidth;
+        float screenHeight = camera.viewportHeight;
+
+        // Define how close to the edge the player must be to move the camera
+
+        System.out.println("Screen Height: "+screenHeight);
+        System.out.println("Snap");
+        // Check and update camera position based on player's direction
+        switch (player.getDirection()) {
+            case UP:
+            case STANDINGUP:
+                camera.position.y += screenHeight*0.3; // Move camera up by one screen height
+                //player.setCurrentWindowY(camera.position.y); // Adjust player's Y position
+                break;
+            case DOWN:
+            case STANDINGDOWN:
+                    camera.position.y -= screenHeight*0.3; // Move camera down by one screen height
+                    //player.setCurrentWindowY(camera.position.y); // Adjust player's Y position
+                break;
+            case LEFT:
+            case STANDINGLEFT:
+                    camera.position.x -= screenWidth*0.3; // Move camera left by one screen width
+                    //player.setCurrentWindowX(camera.position.x); // Adjust player's X position
+                break;
+            case RIGHT:
+            case STANDINGRIGHT:
+                    camera.position.x += screenWidth*0.3; // Move camera right by one screen width
+                    //player.setCurrentWindowX(camera.position.x); // Adjust player's X position
+                break;
+            default:
+                // Do nothing for standing directions
+                break;
+        }
+        camera.update(); // Update the camera after repositioning
+    }
+
+
 
 
     @Override
