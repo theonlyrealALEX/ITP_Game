90e333aa9a308b8d2eca8b0b28510a20f3227e84
Jodie Tsai <jodie.tsai@tum.de>
2023-12-23 17:12:00 +0100

    Added clicked sound, ESC sound effect, fade away animation, new background music (can be changed back)

--- core/src/de/tum/cit/ase/maze/MenuScreen.java
+++ core/src/de/tum/cit/ase/maze/MenuScreen.java
@@ -2,10 +2,12 @@ package de.tum.cit.ase.maze;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -38,7 +40,7 @@ public class MenuScreen implements Screen {
         stage.addActor(table); // Add the table to the stage
 
         // Add a label as a title
-        table.add(new Label("Hello World from the Menu!", game.getSkin(), "title")).padBottom(80).row();
+        table.add(new Label("Welcome to Maze Runner Game!", game.getSkin(), "title")).padBottom(80).row();
 
         // Create and add a button to go to the game screen
         TextButton goToGameButton = new TextButton("Go To Game", game.getSkin());
@@ -46,7 +48,11 @@ public class MenuScreen implements Screen {
         goToGameButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                game.goToGame(); // Change to the game screen when button is pressed
+                Music clickMusic = Gdx.audio.newMusic(Gdx.files.internal("click_sound.mp3"));
+                clickMusic.setVolume(1.5f);
+                clickMusic.setLooping(false);
+                clickMusic.play();
+                game.goToGame();
             }
         });
     }
@@ -78,6 +84,7 @@ public class MenuScreen implements Screen {
     // The following methods are part of the Screen interface but are not used in this screen.
     @Override
     public void pause() {
+
     }
 
     @Override
@@ -87,4 +94,6 @@ public class MenuScreen implements Screen {
     @Override
     public void hide() {
     }
+
+
 }
